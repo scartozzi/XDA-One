85872d2f12e646f8ed2f27a3f15ae8f960119505
BioHaZard1 <15791579a>
2015-06-13 13:24:09 +0100

    base: clean up

--- android/src/main/java/com/xda/one/ui/BaseActivity.java
+++ android/src/main/java/com/xda/one/ui/BaseActivity.java
@@ -2,7 +2,6 @@ package com.xda.one.ui;
 
 import com.xda.one.util.OneApplication;
 
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
