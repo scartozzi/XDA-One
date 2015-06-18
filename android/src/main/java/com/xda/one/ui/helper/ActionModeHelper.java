955b68157794d528f48474e44b73b62cd7212cf7
BioHaZard1 <15791579a>
2015-06-18 21:27:23 +0100

    actionmode: cleanup

--- android/src/main/java/com/xda/one/ui/helper/ActionModeHelper.java
+++ android/src/main/java/com/xda/one/ui/helper/ActionModeHelper.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -132,9 +131,6 @@ public class ActionModeHelper implements View.OnClickListener, View.OnLongClickL
                 menuItem);
     }
 
-    //
-    // ToDo Fix, prevents FC during long press for now
-    //
     public void onCheckedStateChanged(final ActionMode actionMode, int position,
             boolean isNowChecked) {
         mActionModeCallback.onCheckedStateChanged(actionMode, position, isNowChecked);
