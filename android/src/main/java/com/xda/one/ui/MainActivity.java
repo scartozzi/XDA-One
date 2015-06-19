97efb71717a7bf0b4d5a0e822c29b24113538e50
BioHaZard1 <15791579a>
2015-06-19 12:38:16 +0100

    mainactivity: change to gravity compat

--- android/src/main/java/com/xda/one/ui/MainActivity.java
+++ android/src/main/java/com/xda/one/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
@@ -167,7 +168,7 @@ public class MainActivity extends BaseActivity
 
     @Override
     public void onBackPressed() {
-        if (mDrawerLayout.isDrawerOpen(Gravity.START)) {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mDrawerLayout.closeDrawer(Gravity.LEFT);
             return;
         }
