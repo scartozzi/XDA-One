4140f9a1e2f59ba9bea783ed725d67b67ab61499
BioHaZard1 <15791579a>
2015-07-07 20:38:59 +0100

    profile: add toolbar to user profile view

--- android/src/main/java/com/xda/one/ui/UserProfileActivity.java
+++ android/src/main/java/com/xda/one/ui/UserProfileActivity.java
@@ -1,12 +1,16 @@
 package com.xda.one.ui;
 
 import com.xda.one.R;
+import com.xda.one.util.UIUtils;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class UserProfileActivity extends BaseActivity {
 
@@ -25,16 +29,29 @@ public class UserProfileActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame_activity);
 
+        UIUtils.setStatusBarColor(this, getResources().getColor(R.color.colorPrimaryDark), false);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(getResources().getString(R.string.about));
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         if (savedInstanceState == null) {
             final String userId = getIntent().getStringExtra(USER_ID_ARGUMENT);
             final Fragment fragment = UserProfileFragment.createInstance(userId);
             final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.frame_activity_content, fragment).commit();
         }
-
-        // Remove the title and subtitle if any
-        /*final ActionBar actionBar = UIUtils.getSupportActionBar(this);
-        actionBar.setTitle(null);
-        actionBar.setSubtitle(null);*/
     }
 }
